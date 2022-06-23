3804bb8f27bf28c35ef5a4a46d1d9a2101297f55
Sandro Martins da Costa <sandromartinscosta@gmail.com>
2022-06-23 11:42:56 -0300

    Alterações site
    Alterações

--- includes/_dados.php
+++ includes/_dados.php
@@ -2,8 +2,8 @@
 
 $produtos = array(
     0 => array('nome'=>'Capivara Legal','preco'=>'1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'),
-    1 => array('nome'=>'Suricato','preco'=>'','descricao'=>'','imagem'=>''),
-    2 => array('nome'=>'Elefante','preco'=>'','descricao'=>'','imagem'=>''),
+    1 => array('nome'=>'Suricato','preco'=>'200','descricao'=>'Descriçao do Suricato','imagem'=>'suricato.jpg'),
+    2 => array('nome'=>'Elefante','preco'=>'450.00','descricao'=>'Elefante Legal','imagem'=>'elefante.jpg'),
     3 => array('nome'=>'Produto A','preco'=>'','descricao'=>'','imagem'=>''),
     4 => array('nome'=>'Produto B','preco'=>'','descricao'=>'','imagem'=>''),
     5 => array('nome'=>'Produto C','preco'=>'','descricao'=>'','imagem'=>''),
--- index.php
+++ index.php
@@ -4,24 +4,29 @@ include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
 ?>
+<div class="container">
+    <h2>Promoção</h2>
+    <div class="row mt-5">
+        <?php
+        // laco de repeticao para exibir os 3 primeiros produtos
+        for ($i=0; $i < 3 ; $i++) { 
+        ?>
+        <div class="card m-3" style="width: 18rem;">
+            <img src="./content/<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
+                <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
+                <a href="produto-detalhe.php?id=<?php echo $i;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+    </div>    
+</div>
 
-<h1>Home</h1>
 <?php
-for ($i=0; $i < 29; $i++) { 
-    
-    echo $produtos[$i]['nome'];
-    echo $produtos[$i]['preco'];
-    echo '<img src="./content/'.$produtos[$i]['imagem'].'">';
-    echo '<hr>';
-}
-
-
-    
-?>
-
-
-
-<?php
+        
 // include do footer
 include_once './includes/_footer.php';
 ?>
