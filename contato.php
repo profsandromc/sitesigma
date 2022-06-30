24a5b57400387612133bab0bc0ad41fc1b09ac04
Sandro Martins da Costa <103116441+profsandromc@users.noreply.github.com>
2022-06-30 13:09:51 -0300

    Update contato.pgp
    Alterações no contato

--- contato.php
+++ contato.php
@@ -3,9 +3,20 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
-?>
 
+// valida se a variavel post txtNome foi enviada
+if( isset( $_POST['txtNome'] ) ){
+    $nome = strtoupper( $_POST['txtNome'] );
+    echo '$nome";
+}
+?>
 <h1>Contato</h1>
+<form action="./contato.php" method="post">
+    <label for="txtNome">Nome Completo</label>
+    <input type="text" name="txtNome" id="txtNome" required>
+    <input type="submit" value="Cadastrar">
+</form>
+
 
 <?php
 // include do footer
