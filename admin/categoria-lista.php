c8003e67650362559e5a4d26634eaf48c0d8d743
Sandro Martins <103116441+profsandromc@users.noreply.github.com>
2022-07-21 12:37:14 -0300

    Atualização
    Site e banco

---
+++ admin/categoria-lista.php
@@ -0,0 +1,15 @@
+<?php
+// inclui a conexao com o banco de dados
+include_once '../includes/_banco.php';
+// inclui o head e header da pagina admin
+include_once './_header.php';
+// inclui o menu do painel admin
+include_once './_menu.php';
+?>
+
+
+
+<?php
+// inclui o footer do painel admin
+include_once './_footer.php';
+?>
--- index.php
+++ index.php
@@ -10,26 +10,20 @@ include_once './includes/_header.php';
     <div class="row mt-5">
         <?php
         // crio uma variavel que contem SQL executado
-        $sql = "SELECT * FROM categorias WHERE Ativo = 1";
+        $sql = "SELECT * FROM produtos WHERE Ativo = 1 ORDER BY RAND() LIMIT 3   ";
         // executa o comando SQL
         $exec = mysqli_query($conn,$sql);
         // informar a quantidade de registros de dados 
         $numProdutos = mysqli_num_rows($exec);
         // percorre todos os dados extraidos do banco
         while ( $dados = mysqli_fetch_assoc($exec) ) {
-            echo '<h1>'.$dados['Nome'].'</h1>';
-
-        }
-
-        // laco de repeticao para exibir os 3 primeiros produtos
-        for ($i=0; $i < 3 ; $i++) { 
         ?>
         <div class="card m-3" style="width: 18rem;">
-            <img src="./content/<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+            <img src="./content/<?php echo $dados['Imagem'];?>" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
-                <p class="card-text"><?php echo $produtos[$i]['descricao'];?></p>
-                <a href="produto-detalhe.php?id=<?php echo $i;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+                <h5 class="card-title"><?php echo $dados['Nome'];?></h5>
+                <p class="card-text"><?php echo $dados['Descricao'];?></p>
+                <a href="produto-detalhe.php?id=<?php echo $dados['ProdutoID'];?>&tipo=promocao" class="btn btn-primary">Comprar</a>
             </div>
         </div>
         <?php
